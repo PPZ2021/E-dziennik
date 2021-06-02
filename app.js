da629389769770e4ad9dabac588de6f856824ad3
pheasantus <83296497+pheasantus@users.noreply.github.com>
2021-06-02 21:48:31 +0200

    dodanie obsługi www (chyba)

--- app.js
+++ app.js
@@ -64,4 +64,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
+
 module.exports = app;
